afc0afcc057e7314cfc00346ae960e5a52246a9e
ShifatERasulbd <shifaterasulbd@gmail.com>
2025-05-08 13:49:16 +0600

    update final

--- resources/views/backend/left_nav.blade.php
+++ resources/views/backend/left_nav.blade.php
@@ -9,10 +9,10 @@
                 <ul class="sidebar-menu">
                     <li class="menu-header">Dashboard</li>
                     <li class="dropdown">
-                        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>About</span></a>
+                        <a href="#" class="nav-link has-dropdown"><i class="fa-solid fa-fire"></i><span>About</span></a>
                         <ul class="dropdown-menu">
-                            <li><a class="nav-link" href="{{ route('about')}}">All</a></li>
-                            <li class="nav-link"><a class="nav-link" href="{{ route('about.add')}}">Add</a></li>
+                            <li><a class="nav-link" href="{{ route('about')}}">All About</a></li>
+                            <li><a class="nav-link" href="{{ route('about.add')}}">Add About</a></li>
                         </ul>
                     </li>
 
@@ -20,16 +20,16 @@
                     <li class="dropdown">
                         <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Methodology</span></a>
                         <ul class="dropdown-menu">
-                            <li><a class="nav-link" href="{{ route('methodhology')}}">All</a></li>
-                            <li class="nav-link"><a class="nav-link" href="{{ route('add.mehodology')}}">Add</a></li>
+                            <li><a class="nav-link" href="{{ route('methodhology')}}">All Methodology</a></li>
+                            <li><a class="nav-link" href="{{ route('add.mehodology')}}">Add Methodology</a></li>
                         </ul>
                     </li>
 
                     <li class="dropdown">
                         <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Investigation</span></a>
                         <ul class="dropdown-menu">
-                            <li><a class="nav-link" href="{{ route('investigation')}}">All</a></li>
-                            <li class="nav-link"><a class="nav-link" href="{{ route('investigation.add')}}">Add</a></li>
+                            <li><a class="nav-link" href="{{ route('investigation')}}">All Investigation</a></li>
+                            <li><a class="nav-link" href="{{ route('investigation.add')}}">Add Investigation</a></li>
                         </ul>
                     </li>
 
@@ -37,31 +37,50 @@
                     <li class="dropdown">
                         <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Team</span></a>
                         <ul class="dropdown-menu">
-                            <li><a class="nav-link" href="{{ route('team')}}">All</a></li>
-                            <li class="nav-link"><a class="nav-link" href="{{ route('team.add')}}">Add</a></li>
+                            <li><a class="nav-link" href="{{ route('team')}}">All Team</a></li>
+                            <li><a class="nav-link" href="{{ route('team.add')}}">Add Team</a></li>
                         </ul>
                     </li>
+
+
+                    <li class="dropdown">
+                        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Team Category</span></a>
+                        <ul class="dropdown-menu">
+                            <li><a class="nav-link" href="{{ route('team_category')}}">All Team Category</a></li>
+                            <li><a class="nav-link" href="{{ route('team_category.add')}}">Add Team Category</a></li>
+                        </ul>
+                    </li>
+
 
                     <li class="dropdown">
                         <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>News</span></a>
                         <ul class="dropdown-menu">
-                            <li><a class="nav-link" href="{{ route('news')}}">All</a></li>
-                            <li class="nav-link"><a class="nav-link" href="{{ route('news.add')}}">Add</a></li>
+                            <li><a class="nav-link" href="{{ route('news')}}">All News</a></li>
+                            <li><a class="nav-link" href="{{ route('news.add')}}">Add News</a></li>
                         </ul>
                     </li>
 
                     <li class="dropdown">
                         <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Contacts</span></a>
                         <ul class="dropdown-menu">
-                            <li><a class="nav-link" href="{{ route('contact')}}">All</a></li>
-                            <li class="nav-link"><a class="nav-link" href="{{ route('contact.add')}}">Add</a></li>
+                            <li><a class="nav-link" href="{{ route('contact')}}">All Contacts</a></li>
+                            <li><a class="nav-link" href="{{ route('contact.add')}}">Add Contacts</a></li>
                         </ul>
                     </li>
 
                     <li class="dropdown">
-                    <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fas fa-fire"></i><span>Logout</span></a>
+                        <!-- <ul class="dropdown-menu">
+                            <li><a class="nav-link" href="{{ route('contact')}}">All Contacts</a></li>
+                            <li><a class="nav-link" href="{{ route('contact.add')}}">Add Contacts</a></li>
+                        </ul> -->
+                    </li>
+
+
+                    <li class="dropdown">
+                    <!-- <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         Logout
-                    </a>
+                    </a> -->
 
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
